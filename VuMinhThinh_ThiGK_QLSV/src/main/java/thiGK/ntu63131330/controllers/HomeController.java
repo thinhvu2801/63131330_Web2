5975d56918c892328d86e864cda308f60f9928f8
Vũ Minh Thịnh <minhthinhhero@gmail.com>
2024-03-26 15:32:26 +0700

    2nd push

--- VuMinhThinh_ThiGK_QLSV/src/main/java/thiGK/ntu63131330/controllers/HomeController.java
+++ VuMinhThinh_ThiGK_QLSV/src/main/java/thiGK/ntu63131330/controllers/HomeController.java
@@ -1,5 +1,24 @@
 package thiGK.ntu63131330.controllers;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class HomeController {
+	@RequestMapping("/welcome")
+	public String welcome(ModelMap model) {
+		model.addAttribute("msg", "Helo maven");
+		return "viewWelcome";
+	}
+	@RequestMapping("/index")
+	public String index(ModelMap model) {
+		model.addAttribute("msg", "Helo maven");
+		return "viewIndex";
+	}
+	@RequestMapping("/about")
+	public String about(ModelMap model) {
+		model.addAttribute("msg", "Helo maven");
+		return "viewAbout";
+	}
 }
